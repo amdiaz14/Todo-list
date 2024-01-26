e20d9639d071096df803ea06ac577a24e4894e10
amdiaz14 <157774896+amdiaz14@users.noreply.github.com>
2024-01-25 21:22:47 -0600

    Add files via upload
    This project takes a simple to-do list whether that be movies to watch, things to do around the house, or etc. and with a function gives the user a menu on the options for the to-do list to be changed according to what they want to do. Maybe they want to cross off a task, added one,  remove all the tasks completely, or just quit the program altogether this function allows them to do that and more.

---
+++ todolist 2.py
@@ -0,0 +1,57 @@
+#Ava Diaz
+#Todo list 2 
+#7th period
+
+
+#my set to do list that doesnt change unless the user wants it to 
+todolist = ["Tangled",  "Coco", "Princess and the Frog", "Moana"]
+
+#User input/menu 
+#function that displays current to do list to the user 
+def display_todo():
+    print(todolist)
+
+#Function that is the menu and gives the user optiion for what they want to do to their to do list
+def mmenu():
+    print("Options for To-Do list:")
+    print("1. Add a task to the to-do list \n 2. View the current to-do list \n 3. Mark a task as completed \n 4. Remove a task from the to-do list \n 5. Remove all tasks from to-do list \n 6. Sort the list alphabetically \n 7. Print the number of items on the to-do list \n 8. Exit the program")
+    option = input("Option: ")
+    if option == "8" :
+        print("Goodbye")
+        quit()
+    if option == "3" :
+        marr = input("What item would you like to mark?")
+        i = todolist.index(marr)
+        todolist[i] = "¤"
+        print(todolist)
+        mmenu()
+    if option == "2":
+        display_todo()
+        mmenu()
+    if option == "1":
+        addd = input("Please add a task to the list")
+        todolist.append(addd)
+        print(todolist)
+        mmenu()
+    if option == "4":
+        remoo = input("What item would you like to remove?")
+        todolist.remove(remoo)
+        print(todolist)
+        mmenu()
+    if option == "5":
+        todolist.clear()
+        print("List cleared")
+        mmenu()
+    if option == "7":
+        print(len(todolist))
+        mmenu()
+    if option == "6":
+        print(sorted(todolist))
+        mmenu()
+
+
+
+
+        
+#Main
+mmenu()
